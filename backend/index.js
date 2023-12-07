a4eea3235c4349c81aefcb940a30a38f7a145b92
Sidharth Sethi <Sidharth.sherry@gmail.com>
2023-12-07 19:30:01 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,13 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/route');
 
 dotenv.config();
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
